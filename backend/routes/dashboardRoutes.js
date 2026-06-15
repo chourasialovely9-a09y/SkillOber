54888c803b90c8750a778506c28d78b315855609
Lovely Chourasia <24155778@kiit.ac.in>
2026-06-15 16:48:12 +0530

    Added Week 3 backend development

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,17 +1,25 @@
 const express = require("express");
-
+const { authorize } = require("../middlewares/roleMiddleware");
 const {
   getDashboardStats,
   getStudentDashboard,
 } = require("../controllers/dashboardController");
 
+const { protect } = require("../middlewares/authMiddleware");
+
 const router = express.Router();
 
-router.get("/", getDashboardStats);
+router.get(
+  "/",
+  protect,
+  authorize("admin"),
+  getDashboardStats
+);
 
 router.get(
   "/student/:studentId",
+  protect,
+  authorize("student", "admin"),
   getStudentDashboard
 );
-
 module.exports = router;
